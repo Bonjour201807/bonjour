cb29aa5f0a94278bb716fc127f674e6dcc695bfe
张月涛 <zhangyuetao@vic-MacBook-Pro.local>
2018-07-31 12:39:32 +0800

    修改智能助手天气组件，支持指定一周内日期，样式修改为轮播

--- src/components/chatbot/ChatBotWeather.vue
+++ src/components/chatbot/ChatBotWeather.vue
@@ -3,7 +3,7 @@
         <!-- <p>citycode: {{ citycode }}</p>
         <p>维度：{{ lat }}</p>
         <p>经度：{{ lng }}</p> -->
-        <weather-detail :weatherdata="weatherdata" :lifedata="lifedata" v-if="flag"></weather-detail>
+        <weather-detail :weatherdata="weatherdata" :lifedata="lifedata" :weatherdate="weatherdate" v-if="flag"></weather-detail>
     </div>
 </template>
 
@@ -14,32 +14,53 @@ import WeatherDetail from "./ChatBotWeatherDetail";
 
 export default {
   name: "weather",
-  props: ["destination"],
+  props: ["destination", "date"],
   components: { WeatherDetail },
   data() {
     return {
       weatherdata: {},
       lifedata: {},
+      weatherdate: {},
       flag: false
     };
   },
   mounted() {
     this.getWeather();
     this.getLife();
+    // this.getWeatherdate();
   },
+
   methods: {
     getWeather() {
       axios
         .get(
           "https://free-api.heweather.com/s6/weather/forecast?&location=" +
             this.destination +
-            "&key=a861917783c34b61add77450fa82a029"
+            "&key=1acdfb31bc4e465a96b428d43e5367e9"
         )
         .then(res => {
           var res = res.data;
+          // this.$store.state.weatherdata = res;
           this.weatherdata = res;
           console.log(this.weatherdata);
           this.flag = true;
+          var weatherdate_lst = this.date[0].split(" ");
+          var weatherindex_lst = this.date[1].split(" ");
+          var weatherdate = weatherdate_lst[0];
+          // console.log(weatherdate);
+          var weatherindex = parseInt(weatherindex_lst[0]);
+          var arr = this.weatherdata.HeWeather6[0].daily_forecast;
+          // console.log(arr);
+          for (var j = 0; j < arr.length; j++) {
+            // console.log(arr[j].date);
+            if (arr[j].date === weatherdate) {
+              // console.log(arr[j].date);
+              var new_arr = arr.slice(j, j + weatherindex);
+              // console.log(cnew_arr);
+              this.weatherdate = new_arr;
+              console.log(this.weatherdate);
+            }
+          }
         });
     },
     getLife() {
@@ -47,13 +68,27 @@ export default {
         .get(
           "https://free-api.heweather.com/s6/weather/lifestyle?parameters&location=" +
             this.destination +
-            "&key=a861917783c34b61add77450fa82a029"
+            "&key=1acdfb31bc4e465a96b428d43e5367e9"
         )
         .then(res => {
           var res = res.data;
           this.lifedata = res;
           console.log(this.lifedata);
         });
+    },
+    getWeatherdate() {
+      var weatherdate_lst = this.date[0].split(" ");
+      var weatherindex_lst = this.date[1].split(" ");
+      var weatherdate = weatherdate_lst[0];
+      console.log(weatherdate);
+      var weatherindex = parseInt(weatherindex_lst[0]);
+      var arr = weath.HeWeather6[0].daily_forecast;
+      for (j = 0; j < arr.length; j++) {
+        if (arr[j] === weatherdate) {
+          var new_arr = arr[(j, j + weatherindex)];
+          this.weatherdate = new_arr;
+        }
+      }
     }
   }
   //   computed: {
@@ -85,6 +120,7 @@ export default {
 
 <style lang="scss" scoped>
 .weather-container {
-  height: 790px;
+  height: 200px;
+  display: inline-block;
 }
 </style>
